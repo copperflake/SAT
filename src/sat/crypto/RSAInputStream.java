0d4d8fcf6417eec429b8622c32e074cb12869d99
Galedric <galedric@galedric.com>
2012-03-14 09:53:50 +0100

    Nearly working RSA Streams

--- src/sat/crypto/RSAInputStream.java
+++ src/sat/crypto/RSAInputStream.java
@@ -1,16 +1,66 @@
 package sat.crypto;
 
-import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
 
-public class RSAInputStream extends FilterInputStream {
-	public RSAInputStream(InputStream in) {
-		super(in);
+public class RSAInputStream extends InputStream {
+	protected InputStream in;
+	protected RSAKeyPair keys;
+	
+	protected int blockSize;
+	protected int bufferSize;
+	protected int bufferLength = 0;
+	protected int bufferPosition = 0;
+	protected byte[] buffer;
+	
+	public RSAInputStream(InputStream in, RSAKeyPair keys) {
+		this.in = in;
+		this.keys = keys;
+		
+		blockSize = (keys.keyLength() - 1) / 8;
+		bufferSize = blockSize - 2;
+		buffer = new byte[bufferSize];
+	}
+	
+	public int available() throws IOException {
+		return bufferLength-bufferPosition;
+	}
+	
+	protected void load() throws IOException {
+		byte[] block = new byte[blockSize+1];
+		
+		for(int i = 0; i < blockSize+1; i++) {
+			block[i] = (byte) (in.read() & 0xff); 
+		}
+		
+		BigInteger block_bigint = new BigInteger(block);
+		block_bigint = keys.decrypt(block_bigint);
+
+		//System.out.println("<" + block_bigint);
+		
+		block = block_bigint.toByteArray();
+		
+		boolean padding = true;
+		int j = 0;
+		for(int i = 1; i < block.length; i++) {
+			if(padding) {
+				if(block[i] == 0) {
+					padding = false;
+				}
+			} else {
+				buffer[j++] = block[i];
+			}
+		}
+		
+		bufferLength = j;
+		bufferPosition = 0;
 	}
 	
 	public int read() throws IOException {
-		int b = in.read() & 0xff;
-		return b;
+		if(bufferPosition >= bufferLength)
+			load();
+		
+		return buffer[bufferPosition++];
 	}
 }
--- src/sat/crypto/RSAOutputStream.java
+++ src/sat/crypto/RSAOutputStream.java
@@ -1,15 +1,71 @@
 package sat.crypto;
 
-import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.math.BigInteger;
 
-public class RSAOutputStream extends FilterOutputStream {
-	public RSAOutputStream(OutputStream out) {
-		super(out);
+public class RSAOutputStream extends OutputStream {
+	protected OutputStream out;
+	protected RSAKeyPair keys;
+	
+	protected int blockSize;
+	protected int bufferSize;
+	protected int bufferLength = 0;
+	protected byte[] buffer;
+	
+	public RSAOutputStream(OutputStream out, RSAKeyPair keys) {
+		this.out = out;
+		this.keys = keys;
+		
+		blockSize = (keys.keyLength() - 1) / 8;
+		bufferSize = blockSize - 2;
+		buffer = new byte[bufferSize];
 	}
 	
 	public void write(int b) throws IOException {
-		out.write(b);
+		buffer[bufferLength++] = (byte) (b & 0xff);
+		if(bufferLength >= bufferSize)
+			flush();
+	}
+	
+	public void flush() throws IOException {
+		if(bufferLength == 0) return;
+		
+		byte[] block = new byte[blockSize+1];
+		
+		int padding = blockSize-1-bufferLength;
+		for(int i = 0; i < padding; i++) {
+			block[i+1] = 1;
+		}
+		
+		for(int i = 0; i < bufferLength; i++) {
+			block[i+padding+2] = buffer[i];
+		}
+		
+		BigInteger block_bigint = new BigInteger(block);
+		
+		//System.out.println(">" + block_bigint);
+		
+		block_bigint = keys.encrypt(block_bigint);
+		
+		byte[] block_encrypted = block_bigint.toByteArray();
+		padding = block.length-block_encrypted.length;
+		for(int i = 0; i < block.length; i++) {
+			if(i < padding) {
+				block[i] = 0;
+			} else {
+				block[i] = block_encrypted[i-padding];
+			}
+		}
+		
+		out.write(block);
+		out.flush();
+		
+		bufferLength = 0;
+	}
+	
+	public void close() throws IOException {
+		flush();
+		out.close();
 	}
 }
