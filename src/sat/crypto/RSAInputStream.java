e871777659d36b7472a6b0b508fb9a9dce9404ee
Bastien Clément <galedric@galedric.com>
2012-03-15 20:56:50 +0100

    Working RSA implementation

--- src/sat/crypto/RSAInputStream.java
+++ src/sat/crypto/RSAInputStream.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
 
+import sat.EndOfWorldException;
+
 public class RSAInputStream extends InputStream {
     protected InputStream in;
     protected RSAKeyPair keys;
     
     protected int blockSize;
+    
     protected int bufferSize;
     protected int bufferLength = 0;
     protected int bufferPosition = 0;
@@ -18,7 +21,7 @@ public class RSAInputStream extends InputStream {
         this.in = in;
         this.keys = keys;
         
-        blockSize = (keys.keyLength() - 1) / 8;
+        blockSize = keys.keyLength() / 8;
         bufferSize = blockSize - 2;
         buffer = new byte[bufferSize];
     }
@@ -28,33 +31,61 @@ public class RSAInputStream extends InputStream {
     }
     
     protected void load() throws IOException {
-        byte[] block = new byte[blockSize+1];
-        
-        for(int i = 0; i < blockSize+1; i++) {
+    	byte[] block = new byte[blockSize+1]; // Implicit 0 front byte
+    	
+        for(int i = 1; i <= blockSize; i++) {
             block[i] = (byte) (in.read() & 0xff); 
         }
         
-        BigInteger block_bigint = new BigInteger(block);
-        block_bigint = keys.decrypt(block_bigint);
-
-        //System.out.println("<" + block_bigint);
+        block = keys.decrypt(new BigInteger(block)).toByteArray();
         
-        block = block_bigint.toByteArray();
+        // BigInteger outputs:
+        //
+        //    [-----key----] -> l = n
+        //
+        // 1) [0|pad|0|buff] -> l = n
+        // 2)   [pad|0|buff] -> l = n-1
+		// 3)   [0|--buff--] -> l = n-2
+        // 4)     [--buff--] -> l = n-3
         
-        boolean padding = true;
-        int j = 0;
-        for(int i = 1; i < block.length; i++) {
-            if(padding) {
-                if(block[i] == 0) {
-                    padding = false;
-                }
-            } else {
-                buffer[j++] = block[i];
-            }
+        int drop = 0, bufferOffset = -1;
+        switch(blockSize-block.length) {
+        	case 0:
+        		// case 1, then case 2...
+        		drop = 1;
+        		
+        	case 1:
+        		// Padding cannot be '0'
+        		if(block[drop] != 0) {
+        			// case 2
+        			for(int i = 1; i < block.length; i++) {
+        				if(block[drop+i] == 0) {
+        					bufferOffset = drop+i+1;
+        					break;
+        				}
+        			}
+        		} else {
+        			// case 3
+        			bufferOffset = 1;
+        		}
+        		break;
+        	
+        	case 2:
+        		// case 4
+        		bufferOffset = 0;
+        		break;
         }
         
-        bufferLength = j;
+        // No-match -> bad block
+        if(bufferOffset < 0)
+        	throw new IOException("Invalid RSA block");
+        
+        // Buffer-reset
+        bufferLength = block.length-bufferOffset;
         bufferPosition = 0;
+        
+        // Copy bytes
+        System.arraycopy(block, bufferOffset, buffer, 0, bufferLength);
     }
     
     public int read() throws IOException {
