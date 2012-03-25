5b7e12966ad8459229b29d4ac3a062317618900a
Sacha Bron <sacha.bron@copperflake.com>
2012-03-25 16:45:14 +0200

    File Transfert almost complete (hash algo is missing).

--- src/sat/file/SegmentableFile.java
+++ src/sat/file/SegmentableFile.java
@@ -3,17 +3,26 @@ package sat.file;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Iterator;
 
 public class SegmentableFile implements Iterable<FileSegment> {
 	private int segmentsCount = 0;
 	private String pathname;
 	private RandomAccessFile file;
+	private byte[] hash;
 	public static final int SEGMENT_SIZE = 1024;
-
+	
 	public SegmentableFile(String pathname) throws FileNotFoundException {
 		this.pathname = pathname;
 		this.file = new RandomAccessFile(this.pathname, "rw");
+		try {
+			this.calculateHash();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public Iterator<FileSegment> iterator() {
@@ -37,6 +46,10 @@ public class SegmentableFile implements Iterable<FileSegment> {
 			public void remove() {
 				// Not available !
 			}
+			
+			public int getCounter() {
+				return currentSegment;
+			}
 		};
 	}
 
@@ -48,5 +61,40 @@ public class SegmentableFile implements Iterable<FileSegment> {
 
 	public void writeSegment(int offset, FileSegment data) throws IOException {
 		file.write(data.getData(), offset * SEGMENT_SIZE, SEGMENT_SIZE);
+	}
+	
+	public byte[] calculateHash() throws NoSuchAlgorithmException {
+		MessageDigest digest = MessageDigest.getInstance("MD5");
+		return null;
+		// return digest.digest(this.file.getContent()); //Hmm... This method can overload the RAM.
+	}
+	
+	public byte[] getHash() {
+		return this.hash;
+	}
+	
+	public String getFormat() {
+		String name = this.pathname;
+		int dotOffset = -1;
+		
+		for(int i = name.length()-1; i >= 0; i--) {
+			if(name.charAt(i) == '.') {
+				dotOffset = i;
+				break;
+			}
+		}
+		
+		if(dotOffset == -1)
+			return "undefined";
+		else {
+			String format = "";
+			for(int i = dotOffset+1; i < name.length(); i++)
+				format += name.charAt(i);
+			return format;
+		}
+	}
+	
+	public long getSize() throws IOException {
+		return this.file.length();
 	}
 }
--- src/sat/radio/Radio.java
+++ src/sat/radio/Radio.java
@@ -4,12 +4,14 @@ import java.io.FileNotFoundException;
 
 import sat.file.SegmentableFile;
 import sat.radio.message.Message;
+import sat.radio.message.MessageData;
 
 /**
  * Une radio non-spécialisée (ni client, ni serveur). Cette classe fourni les
  * outils communs utilisés à la fois par les serveurs-radio et les
  * client-radios.
  */
+
 public abstract class Radio {
 	/**
 	 * L'identifiant de cette radio. Tous les éléments d'un réseau radio SAT
@@ -17,8 +19,11 @@ public abstract class Radio {
 	 */
 	protected RadioID id;
 
-	public void file(String path, String dest) throws FileNotFoundException {
+	public void sendFile(String path, String dest) throws FileNotFoundException {
 		SegmentableFile file = new SegmentableFile(path);
+		while(file.iterator().hasNext()) {
+			MessageData message = new MessageData(id, px, py, file.getHash(), file.iterator().getCounter(), file.getFormat(), file.getSize(), file.iterator().next());
+		}
 	}
 
 	protected void send(Message msg, String dest) {
