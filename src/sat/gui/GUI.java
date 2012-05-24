2dcb5a23abe5df66822413ae5d52e690a9edd119
Galedric <g@ledric.me>
2012-05-24 09:54:42 +0200

    Fixin errors

--- src/sat/gui/GUI.java
+++ src/sat/gui/GUI.java
@@ -25,12 +25,13 @@ import sat.tower.TowerEvent;
 import sat.tower.agent.TowerAgent;
 
 /**
- * Cette classe va initialiser le GUI, l'afficher et récupérer les évenements émis par le ToweeAgent.
- * Elle créera une ou plusieurs fenêtres selon la taille de l'écran de l'utilisateur.
+ * Cette classe va initialiser le GUI, l'afficher et récupérer les évenements
+ * émis par le ToweeAgent. Elle créera une ou plusieurs fenêtres selon la taille
+ * de l'écran de l'utilisateur.
  */
 public class GUI extends JFrame implements EventListener {
 	private static final long serialVersionUID = -6319538639673860639L;
-	
+
 	public static HashMap<RadioID, Aircraft> aircrafts;
 	private Radar radar;
 	private TowerAgent agent;
@@ -38,10 +39,12 @@ public class GUI extends JFrame implements EventListener {
 	private JournalPanel journalPanel;
 	private DownloadPanel downloadPanel;
 	private boolean enable3D = true;
-	
+
 	/**
-	 * Le constructeur crée ou non un onglet contenant la vue tridimentionnelle selon les paramètres du constructeur.
-	 * Le paramètre HD permet de définir si la vue 3D doit être de haute qualité ou non.
+	 * Le constructeur crée ou non un onglet contenant la vue tridimentionnelle
+	 * selon les paramètres du constructeur. Le paramètre HD permet de définir
+	 * si la vue 3D doit être de haute qualité ou non.
+	 * 
 	 * @param hd
 	 * @param agent
 	 * @param enable3D
@@ -50,78 +53,84 @@ public class GUI extends JFrame implements EventListener {
 		// Create a window. The program will exit when the window is closed.
 		// See http://docs.oracle.com/javase/tutorial/uiswing/components/frame.html
 		super("Airport");
-		
+
 		this.enable3D = enable3D;
 
 		this.agent = agent;
 		agent.addListener(this);
-		
+
 		aircrafts = new HashMap<RadioID, Aircraft>();
 
 		int width = 1500;
 		int height = 950;
 		int topRowHeight = 783;
 		setSize(width, height);
-		
+
 		// Pop the window in the middle of the screen. (Work correctly on a dual-screen btw.)
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		/**
-		 * La classe Toolkit va nous permettre de récupérer la taille de l'écran.
+		 * La classe Toolkit va nous permettre de récupérer la taille de
+		 * l'écran.
 		 */
-		Toolkit toolkit = Toolkit.getDefaultToolkit ();
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = toolkit.getScreenSize();
-		
+
 		AirportPanel airportPanel = new AirportPanel();
 		airportPanel.setPreferredSize(airportPanel.getBackgroundDimension());
 
 		journalPanel = new JournalPanel();
 		ChokerPanel chockerPanel = new ChokerPanel();
-		
+
+		if(!agent.isRemote()) {
+			downloadPanel = new DownloadPanel();
+		}
+		else {
+			JLabel label = new JLabel("<html>This is a remote interface.<br>If you want to see files, you have to check<br>manually on the server.</html>", SwingConstants.CENTER);
+			remoteMsg.add(Box.createVerticalStrut(200));
+			remoteMsg.add(label);
+			remoteMsg.add(Box.createVerticalGlue());
+		}
+
 		final JTabbedPane tabbedPane = new JTabbedPane();
 		tabbedPane.addTab("2D View", airportPanel);
-		
+
 		if(screenSize.getWidth() >= width && screenSize.getHeight() >= height) {
 			Box topRow = Box.createHorizontalBox();
 			Box botRow = Box.createHorizontalBox();
 			Box main = Box.createVerticalBox();
-	
+
 			// Un bout de code affreux, mais c'est belle est bien le seul moyen d'obtenir le résultat esconté.
 			// Merci Swing.
 			topRow.setPreferredSize(new Dimension(topRow.getWidth(), topRowHeight));
-			botRow.setPreferredSize(new Dimension(botRow.getWidth(), getHeight()-topRowHeight));
-			
+			botRow.setPreferredSize(new Dimension(botRow.getWidth(), getHeight() - topRowHeight));
+
 			topRow.add(tabbedPane);
 			topRow.add(Box.createHorizontalStrut(5));
 
-			
 			if(!agent.isRemote()) {
-				downloadPanel = new DownloadPanel();
 				topRow.add(downloadPanel);
 			}
 			else {
 				remoteMsg = new JPanel();
 				remoteMsg.setLayout(new BoxLayout(remoteMsg, BoxLayout.Y_AXIS));
-				JLabel label = new JLabel("<html>This is a remote interface.<br>If you want to see files, you have to check<br>manually on the server.</html>", SwingConstants.CENTER);
-				remoteMsg.add(Box.createVerticalStrut(200));
-				remoteMsg.add(label);
-				remoteMsg.add(Box.createVerticalGlue());
+
 				if(screenSize.getWidth() >= width && screenSize.getHeight() >= height) {
 					remoteMsg.setPreferredSize(new Dimension(380, topRowHeight));
 				}
 				topRow.add(remoteMsg);
 			}
-			
+
 			botRow.add(journalPanel);
 			botRow.add(chockerPanel);
-	
+
 			main.add(topRow);
 			main.add(Box.createVerticalStrut(5));
 			main.add(botRow);
 			setContentPane(main);
-	
+
 			setVisible(true);
 		}
 		else {
@@ -130,11 +139,14 @@ public class GUI extends JFrame implements EventListener {
 			journalFrame.setSize(700, 500);
 			journalFrame.setVisible(true);
 
-			JFrame downloadFrame = new JFrame();
-			downloadFrame.add(downloadPanel);
-			downloadFrame.setSize(500, 600);
-			downloadFrame.setVisible(true);
-
+			if(!agent.isRemote()) {
+				JFrame downloadFrame = new JFrame();
+				downloadFrame.add(downloadPanel);
+				downloadFrame.add(downloadPanel);
+				downloadFrame.setSize(500, 600);
+				downloadFrame.setVisible(true);
+			}
+			
 			JFrame tabbedFrame = new JFrame();
 			tabbedFrame.add(tabbedPane);
 			tabbedFrame.setSize(new Dimension(1116, 779));
@@ -143,32 +155,32 @@ public class GUI extends JFrame implements EventListener {
 			tabbedFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 			tabbedFrame.setVisible(true);
 		}
-		
+
 		if(enable3D) {
 			/**
 			 * Initialise, configure et lance la vue 3D dans un nouvel onglet.
-			 * Doit être invoquer une fois que le reste du GUI est prêt (selon la doc de jMonkeyEngine). 
+			 * Doit être invoquer une fois que le reste du GUI est prêt (selon
+			 * la doc de jMonkeyEngine).
 			 */
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
 					Dimension dim = new Dimension(1109, 751);
-	
+
 					AppSettings settings = new AppSettings(true);
 					settings.setWidth((int) dim.getWidth());
 					settings.setHeight((int) dim.getHeight());
 					settings.setFrameRate(40);
-					settings.setSamples(hd?4:0);
-					
-					
+					settings.setSamples(hd ? 4 : 0);
+
 					radar = new Radar(hd);
 					radar.setSettings(settings);
 					radar.createCanvas();
 					radar.startCanvas(true);
-	
+
 					JmeCanvasContext ctx = (JmeCanvasContext) radar.getContext();
 					ctx.setSystemListener(radar);
 					ctx.getCanvas().setPreferredSize(dim);
-					
+
 					tabbedPane.addTab("3D View", ctx.getCanvas());
 					tabbedPane.setVisible(true);
 				}
@@ -189,7 +201,7 @@ public class GUI extends JFrame implements EventListener {
 		if(enable3D && radar != null) {
 			aircrafts.get(e.getID()).destroy();
 		}
-		
+
 		aircrafts.remove(e.getID());
 	}
 
@@ -199,16 +211,16 @@ public class GUI extends JFrame implements EventListener {
 
 	public void on(RadioEvent.PlaneDistress e) {
 		aircrafts.get(e.getID()).setDistress2D();
-		
+
 		if(enable3D && radar != null) {
 			aircrafts.get(e.getID()).setDistress3D(true);
 		}
 	}
-	
+
 	public void on() {
-		 //downloadPanel.addFilesToDownloadBox(fileList);
+		//downloadPanel.addFilesToDownloadBox(fileList);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void on(Message m) {
 		@SuppressWarnings("rawtypes")
